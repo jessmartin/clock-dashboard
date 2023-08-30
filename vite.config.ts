99b1e9f0d9a8d09b279cd0be8528a6ef3f52fe18
Jess Martin <jessmartin@gmail.com>
2023-08-30 17:35:48 -0400

    Fix build bug

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import { ConfigPlugin } from "@dxos/config/vite-plugin";
 import { VaultPlugin } from "@dxos/vault/vite-plugin";
 import react from "@vitejs/plugin-react";
+import { ThemePlugin } from "@dxos/aurora-theme/plugin";
+import { resolve } from "path";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
@@ -16,7 +18,16 @@ export default defineConfig({
   plugins: [
     VaultPlugin(),
     ConfigPlugin(),
-    react(),
+    react({ jsxRuntime: "classic" }),
+    ThemePlugin({
+      content: [
+        resolve(__dirname, "./index.html"),
+        resolve(__dirname, "./src/**/*.{js,ts,jsx,tsx}"),
+        resolve(__dirname, "node_modules/@dxos/aurora/dist/**/*.mjs"),
+        resolve(__dirname, "node_modules/@dxos/aurora-theme/dist/**/*.mjs"),
+        resolve(__dirname, "node_modules/@dxos/react-appkit/dist/**/*.mjs"),
+      ],
+    }),
     VitePWA({
       registerType: "prompt",
       workbox: {
